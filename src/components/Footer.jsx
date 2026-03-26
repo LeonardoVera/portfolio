5b5488f5ad5a9f18197799b529df350459881add
LeonardoVera <leonardovera20037@gmail.com>
2026-03-26 04:54:26 -0500

    Add Footer component and styles for contact information

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+export function Footer() {
+    return (
+        <footer>
+            <div className="content">
+                <h2>Contact</h2>
+                <p>Esto es una prueba</p>
+                <small>leonardovera20037@gmail.com</small>
+            </div>
+        </footer>
+    )
+}
